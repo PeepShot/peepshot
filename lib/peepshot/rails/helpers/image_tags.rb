60e885f30a19880a82ce38a0e884d26c3bb77ce8
Anders Toxboe <anders@toxboe.net>
2012-02-14 22:28:23 +0100

    Allow for other image_tag options than width and heigt - for instance class, id, etc.

--- lib/peepshot/rails/helpers/image_tags.rb
+++ lib/peepshot/rails/helpers/image_tags.rb
@@ -5,7 +5,7 @@ module PeepShot
         def peepshot_image_tag(url, options)
           width = find_width(options).to_s.downcase == 'original' ? 1024 : find_width(options)
           height = find_height(options)
-          image_tag(peepshot_url(url, options), :width => width, :height => height)
+          image_tag(peepshot_url(url, options), {:width => width, :height => height}.merge(options.except(:width, :height)))
         end
       end
     end
